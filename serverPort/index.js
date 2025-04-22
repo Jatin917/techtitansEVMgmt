d00d6e19b7e60c68453ff64cfd93cc7db1446572
Jatin917 <jkchandel02@gmail.com>
2025-04-22 21:54:06 +0530

    project initialized

--- serverPort/index.js
+++ serverPort/index.js
@@ -12,10 +12,15 @@ app.get('/', ()=>{
     res.send("<h1>server port is running</h1>")
 })
 
-
 app.listen(3000, ()=>{
     console.log("server is running")
 })
 
 connection(DB_URL);
 
+// if by any chance the server will crash, port will not charge any vehicle
+setInterval(async ()=>{
+    await fetch('http://localhost:8080/status',{
+        method:"POST",
+    })
+},10000)
